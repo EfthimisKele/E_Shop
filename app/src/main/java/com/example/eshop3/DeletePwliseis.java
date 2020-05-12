0ed9334edfbdfef83cd20ae3cb1d8bc446510c9d
EfthimisKele <m.efthimis4@gmail.com>
2020-05-12 20:59:05 +0300

    Delete Pwliseis now works updating table Proionta

--- app/src/main/java/com/example/eshop3/DeletePwliseis.java
+++ app/src/main/java/com/example/eshop3/DeletePwliseis.java
@@ -11,8 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class DeletePwliseis extends Fragment {
-    EditText delete_txtpr;
+    EditText d1,d2,d3,d4,d5,d6;
     Button deletePwl;
 
     public DeletePwliseis() {
@@ -28,23 +30,114 @@ public class DeletePwliseis extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_delete_pwliseis, container, false);
-        delete_txtpr = view.findViewById(R.id.delete_txtPwl);
+        d1 = view.findViewById(R.id.del_textPwl1);
+        d2 = view.findViewById(R.id.del_textPwl2);
+        d3 = view.findViewById(R.id.del_textPwl3);
+        d4 = view.findViewById(R.id.del_textPwl4);
+        d5 = view.findViewById(R.id.del_textPwl5);
+        d6 = view.findViewById(R.id.del_textPwl6);
         deletePwl = view.findViewById(R.id.deletePwl);
         deletePwl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int Var_ppid = 0;
+                int pwl_id = 0;
                 try {
-                    Var_ppid = Integer.parseInt(delete_txtpr.getText().toString());
-                } catch (NumberFormatException ex){
+                    pwl_id = Integer.parseInt(d1.getText().toString());
+                } catch (NumberFormatException ex) {
                     System.out.println("Could not parse" + ex);
                 }
-                Pwliseis pwliseis = new Pwliseis();
-                pwliseis.setPpid(Var_ppid);
-                MainActivity.myAppDatabase.myDao().deletePwliseis(pwliseis);
-                Toast.makeText(getActivity(), "Η αγορές με αυτό το id διαγράφθηκαν", Toast.LENGTH_LONG).show();
-                delete_txtpr.setText("");
-            }
+                String pwl_name = d2.getText().toString();
+                int pwlA = 0;
+                try {
+                    pwlA = Integer.parseInt(d3.getText().toString());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Could not parse" + ex);
+                }
+                int pwlB = 0;
+                try {
+                    pwlB = Integer.parseInt(d4.getText().toString());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Could not parse" + ex);
+                }
+                int pwlC = 0;
+                try {
+                    pwlC = Integer.parseInt(d5.getText().toString());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Could not parse" + ex);
+                }
+                int pwlD = 0;
+                try {
+                    pwlD = Integer.parseInt(d6.getText().toString());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Could not parse" + ex);
+                }
+                if (pwl_name.equals("")) {
+                    String m = "Δεν έβαλες όνομα";
+                    Toast.makeText(getActivity(), m, Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        List<Proionta> proionta1 = MainActivity.myAppDatabase.myDao().getProionta();
+                        for (Proionta i: proionta1) {
+                            Integer p_id = i.getPid();
+                            Integer posotita = i.getPosotita();
+                            Integer xronologia = i.getXronologia();
+                            Integer timi = i.getTimi();
+                            Integer diafora = 0;
+                            if (p_id == 1) {
+                                    diafora = (posotita + pwlA);
+                                    Proionta proionta = new Proionta();
+                                    proionta.setPid(p_id);
+                                    proionta.setPosotita(diafora);
+                                    proionta.setXronologia(xronologia);
+                                    proionta.setTimi(timi);
+                                    MainActivity.myAppDatabase.myDao().updateProion(proionta);
+                            }else if (p_id == 2){
+                                diafora = (posotita + pwlB);
+                                    Proionta proionta = new Proionta();
+                                    proionta.setPid(p_id);
+                                    proionta.setPosotita(diafora);
+                                    proionta.setXronologia(xronologia);
+                                    proionta.setTimi(timi);
+                                    MainActivity.myAppDatabase.myDao().updateProion(proionta);
+                            }else if (p_id == 3){
+                                diafora = (posotita + pwlC);
+                                    Proionta proionta = new Proionta();
+                                    proionta.setPid(p_id);
+                                    proionta.setPosotita(diafora);
+                                    proionta.setXronologia(xronologia);
+                                    proionta.setTimi(timi);
+                                    MainActivity.myAppDatabase.myDao().updateProion(proionta);
+                            }else if (p_id == 4){
+                                diafora = (posotita + pwlD);
+                                    Proionta proionta = new Proionta();
+                                    proionta.setPid(p_id);
+                                    proionta.setPosotita(diafora);
+                                    proionta.setXronologia(xronologia);
+                                    proionta.setTimi(timi);
+                                    MainActivity.myAppDatabase.myDao().updateProion(proionta);
+                            }
+                        }
+                        Pwliseis pwliseis = new Pwliseis();
+                        pwliseis.setOnoma(pwl_name);
+                        pwliseis.setPosoA(pwlA);
+                        pwliseis.setPosoB(pwlB);
+                        pwliseis.setPosoC(pwlC);
+                        pwliseis.setPosoD(pwlD);
+                        pwliseis.setPpid(pwl_id);
+                        MainActivity.myAppDatabase.myDao().deletePwliseis(pwliseis);
+                        Toast.makeText(getActivity(), "Η αγορά με αυτό το id διαγράφθηκαν", Toast.LENGTH_LONG).show();
+                    }catch (Exception e) {
+                            String message = e.getMessage();
+                            Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+                  }
+                //Στο τελος αδειαζω τα EditText
+                d1.setText("");
+                d2.setText("");
+                d3.setText("");
+                d4.setText("");
+                d5.setText("");
+                d6.setText("");
+            }     }
         });
         return view;
     }
